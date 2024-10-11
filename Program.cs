b7015411c3339f08d38369b9109970ee1635d120
chris <stickwalkerdev@gmail.com>
2024-10-11 09:58:00 -0400

    Error Handling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the calculator");
+            double num1;
+            double num2;
+            double result;
+
+            Console.WriteLine("Enter Number 1:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Number 2:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Please Enter Operation: +,   -,  *,  /");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid operation");
+                    break;
+
+            }
         }
     }
 }
